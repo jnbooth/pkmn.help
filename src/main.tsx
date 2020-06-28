475123a44702cefe8beeda44ffed3e82ab573c8f
Brian Mock <brian@mockbrian.com>
2020-06-28 13:31:04 -0700

    idk pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as OfflinePluginRuntime from "offline-plugin/runtime";
 import SmoothScroll from "smoothscroll-polyfill";
 
 import "tachyons/css/tachyons.css";
 import "./style.css";
 import { App } from "./App";
 
+OfflinePluginRuntime.install({
+  onInstalled: () => {
+    alert("App was installed!");
+  },
+});
 SmoothScroll.polyfill();
 ReactDOM.render(
   <React.StrictMode>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OfflinePlugin = require("offline-plugin");
 
 // Hack to put the NODE_ENV to production when we ask for minified code.
 const isProd = process.argv.indexOf("-p") >= 0;
@@ -10,14 +11,14 @@ const fileLoader = {
   test: /\.(png|svg)$/,
   loader: "file-loader",
   options: {
-    name: "files/[path][name].[contenthash:8].[ext]"
+    name: "files/[path][name].[contenthash:8].[ext]",
   },
-  include: [path.resolve(__dirname, "img"), path.resolve(__dirname, "svg")]
+  include: [path.resolve(__dirname, "img"), path.resolve(__dirname, "svg")],
 };
 
 const cssLoader = {
   test: /\.css$/,
-  use: ["style-loader", "css-loader"]
+  use: ["style-loader", "css-loader"],
   // We need to load Tachyons which is in node_modules
   // include: path.resolve(__dirname, "src")
 };
@@ -26,32 +27,33 @@ const tsLoader = {
   // Not actually loading any JS files any more, but whatever
   test: /\.[tj]sx?$/,
   loader: "ts-loader",
-  include: path.resolve(__dirname, "src")
+  include: path.resolve(__dirname, "src"),
 };
 
 module.exports = {
   entry: [path.join(__dirname, "src/main.tsx")],
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devtool: "source-map",
   devServer: {
     contentBase: __dirname,
-    compress: true
+    compress: true,
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
   },
   module: {
-    rules: [tsLoader, cssLoader, fileLoader]
+    rules: [tsLoader, cssLoader, fileLoader],
   },
   plugins: [
     new webpack.EnvironmentPlugin(["NODE_ENV"]),
     new HtmlWebpackPlugin({
       filename: path.join(__dirname, "dist/index.html"),
       template: path.join(__dirname, "template/index.html"),
-      hash: true
-    })
-  ]
+      hash: true,
+    }),
+    new OfflinePlugin(),
+  ],
 };
